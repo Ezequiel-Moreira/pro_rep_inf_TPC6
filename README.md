0116a41270b140702d6ba6815e5056411c12eb8b
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-10-27 15:05:12 +0100

    secound commit: adicionar estrutura base ao servidor

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TPC6
 
-Resolução do TPC6
+Resolução do TPC6 da disciplina de Processamento e Representação de dados
--- server.js
+++ server.js
@@ -6,3 +6,41 @@ var {parse} = require('querystring')
 var jsonfile = require('jsonfile')
 
 var porta = 7000
+var bd = "data/teses.json"
+
+var server=http.createServer((req,res)=>{
+  var parsedUrl = url.parse(req.url,true)
+  var pathname = parsedUrl.pathname
+
+  if(req.method == 'GET'){
+    if(pathname == '/'){
+
+    }else if(pathname == '/registo'){
+
+    }else if(pathname == '/lista'){
+      
+    }else if(pathname == '/processaForm'){
+
+    }else if(pathname == '/w3.css'){
+
+    }else{
+      res.writeHead(501, {'Content-type' : 'text/html;charset=utf-8'})
+      res.end('Erro: ' + parsedUrl.pathname + ' não está implementado')
+    }
+  }else if(req.method == 'POST'){
+    if(pathname == '/processaForm'){
+      
+    }else{
+      res.writeHead(501, {'Content-type' : 'text/html;charset=utf-8'})
+      res.end('Erro: ' + parsedUrl.pathname + ' não está implementado')
+    }
+  }else{
+    res.writeHead(503, {'Content-type' : 'text/html;charset=utf-8'})
+    res.end('Erro: ' + req.method + ' não está implementado')
+  }
+})
+
+
+server.listen(porta,()=>{
+  console.log("À escuta na porta " + porta)
+})
